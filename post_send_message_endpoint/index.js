2462a4be63eced4e63adee50515915da56e1d1dd
901david <vdavidhammond@gmail.com>
2020-02-16 06:18:42 -0700

    updated api for post

--- post_send_message_endpoint/index.js
+++ post_send_message_endpoint/index.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context, callback) => {
     await twilio.messages.create({
       from,
       to,
-      body
+      body: body.message
     });
 
     callback(undefined, {
